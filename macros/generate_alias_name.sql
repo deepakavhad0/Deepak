6b151792c588439078599bf1496e4ad5d4cc8e5e
Deepak Avhad <deepak.avhad@outlook.com>
2024-11-13 14:04:48 +0000

    commit

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -6,12 +6,4 @@
     {%- else -%} {{ node.name }}
     {%- endif -%}
 {%- endmacro %}
-=======
-{% macro generate_alias_name(custom_alias_name=none, node=none) -%}
-    {%- if custom_alias_name -%} {{ custom_alias_name | trim }}
-    {%- elif node.version -%}
-        {{ return(node.name ~ "_v" ~ (node.version | replace(".", "_"))) }}
-    {%- else -%} {{ node.name }}
-    {%- endif -%}
-{%- endmacro %}
->>>>>>> e81ff59ad9371d01535cf18332c2fb1b7f5b0771
+
